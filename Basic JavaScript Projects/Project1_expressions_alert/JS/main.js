0cb7ddb482b1dce32fd98875b12c6424abb3ffe8
tttthecoder <trungtintran1905@gmail.com>
2023-07-18 20:59:43 -0600

    Added Project1_expressions_alert folder.

---
+++ Basic JavaScript Projects/Project1_expressions_alert/JS/main.js
@@ -0,0 +1,24 @@
+
+// this is for the alert assignment.
+window.alert('Hello world')
+alert('Hello world')
+
+// this is for the string concatenation assignemnt.
+var sent1 = "This is the beginning of the string", sent2 = " and this is the end of the string"
+var concatenated_text = sent1 + sent2
+document.write(sent1+sent2)
+
+// this is for the multiple variable assignment assignment. 
+var dad  = "Joseph", son = 'Peter', mother = 'Jessica' 
+document.write('<br> ',dad)
+
+// this is for the operation assignment. 
+var hourlyRate = 15, hoursWorked = 20
+Paycheck = hoursWorked * hourlyRate
+document.write("<br><br>This is your pay for this paycheck: CAD",Paycheck)
+
+
+
+
+
+

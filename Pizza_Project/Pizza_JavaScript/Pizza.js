5dc2ab478cce3506d87423dff660cd755278eb52
tttthecoder <trungtintran1905@gmail.com>
2023-08-04 12:07:16 -0600

    added the pizza folder project

---
+++ Pizza_Project/Pizza_JavaScript/Pizza.js
@@ -0,0 +1,69 @@
+
+function getReceipt() {
+    // this initializes our string so it can get passed from function to function, growing line by line into a full receipt.
+    var text1 = "<h3>You ordered:</h3>"
+    var runningTotal = 0;
+    var sizeTotal = 0
+    var sizeArray = document.getElementsByClassName("size")
+    for (var i = 0; i < sizeArray.length; i++) {
+        if (sizeArray[i].checked) {
+            var selectedSize = sizeArray[i].value;
+            text1 = text1 + selectedSize + "<br>"
+        }
+    }
+    // this set the sizeTotal to the corresponding selected size price.
+    if (selectedSize === "Personal Pizza") {
+        sizeTotal = 6
+
+    }
+    else if (selectedSize === "Small Pizza") {
+        sizeTotal = 8
+    }
+    else if (selectedSize === "Medium Pizza") {
+        sizeTotal = 10
+    }
+    else if (selectedSize === "Large Pizza") {
+        sizeTotal = 14
+    }
+    else if (selectedSize === "Extra Large Pizza") {
+        sizeTotal = 16
+    }
+    runningTotal = sizeTotal;
+    console.log(selectedSize + " = $" + sizeTotal + '.00')
+    console.log("size text1: " + text1)
+    console.log("Subtotal: " + runningTotal + '.00')
+    // these variables will get passed on to each function
+    getTopping(runningTotal, text1)
+}
+
+function getTopping(runningTotal, text1) {
+    // this variable is the subtotal for the toppings added.
+    var toppingTotal = 0
+    // create a variable to keep track of all the toppings.
+    var selectedTopping = []
+    var toppingArray = document.getElementsByClassName("toppings")
+    // loops through the array.
+    for (var j = 0; j < toppingArray.length; j++) {
+        // check if the button is checked
+        if (toppingArray[j].checked) {
+            selectedTopping.push(toppingArray[j].value)
+            console.log("selected topping item: (" + toppingArray[j].value + ")")
+            text1 = text1 + toppingArray[j].value + "<br>"
+        }
+    }
+    // calculate the toppings total
+    var toppingCount = selectedTopping.length
+    if (toppingCount > 1) {
+        toppingTotal = (toppingCount - 1)
+    } else {
+        toppingTotal = 0
+    }
+    runningTotal += toppingTotal
+    console.log("total selected topping items: " + toppingCount)
+    console.log(toppingCount + "topping -1 free topping = " + "$" + toppingTotal + '.00')
+    console.log("topping tex1: " + text1)
+    console.log('Purchase Total: ' + runningTotal + '.00')
+    // display the order and receipt to the screen.
+    document.getElementById('showText').innerHTML = text1
+    document.getElementById('totalPrice').innerHTML = "<h3>Total: <strong>$" + runningTotal + '.00' + "</strong></h3>"
+}
